984d99afdfb85291f92ce88d831171c66b32d1fc
RayzorST <mihailcherry@vk.com>
2023-09-13 16:55:00 +0300

    Controllers

---
+++ src/main/java/com/mirea/web/controllers/RegistrationController.java
@@ -0,0 +1,14 @@
+package com.mirea.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegistrationController {
+    @GetMapping("/registration")
+    public String registraion(Model model){
+        model.addAttribute("title", "Не Главная страница");
+        return "registration";
+    }
+}
