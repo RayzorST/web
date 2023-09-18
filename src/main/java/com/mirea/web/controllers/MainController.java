f44b82fb824f49c275dd58925211f8795cb136a6
RayzorST <mihailcherry@vk.com>
2023-09-18 11:39:27 +0300

    main

--- src/main/java/com/mirea/web/controllers/MainController.java
+++ src/main/java/com/mirea/web/controllers/MainController.java
@@ -1,14 +1,21 @@
 package com.mirea.web.controllers;
 
+import com.mirea.web.models.Cards;
+import com.mirea.web.repo.CardsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+    @Autowired
+    private CardsRepository cardsRepository;
+
     @GetMapping("/")
     public String home(Model model){
-        model.addAttribute("title", "Главная страница");
+        Iterable<Cards> cards = cardsRepository.findAll();
+        model.addAttribute("cards", cards);
         return "home";
     }
 }
